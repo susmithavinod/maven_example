27fc9285602f51dd398d569bf725d0df0f833812
susmithavinod <vinodsusmitha@gmail.com>
2023-08-02 18:07:33 +0000

    Update snapper.java

--- src/main/java/snapper.java
+++ src/main/java/snapper.java
@@ -2,4 +2,15 @@ public class snapper {
     public static void main(String[] args) {
         System.out.println("hi from maven project");
     }
+
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            char firstChar = inputString.charAt(0);
+            char lastChar = inputString.charAt(inputString.length() - 1);
+            String mid = inputString.substring(1, inputString.length() - 1);
+            return (firstChar == lastChar) && isPalindrome(mid);
+        }
+    }
 }
